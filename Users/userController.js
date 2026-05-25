5cd2304eda01b22fd7198ada58b955d116d5f876
Yadhuveer <yadhuveeryadu@gmail.com>
2026-05-25 12:51:22 +0530

    change domain1

--- Users/userController.js
+++ Users/userController.js
@@ -77,7 +77,7 @@ export class UserController{
             sameSite: 'lax' 
         });
         
-             console.log("token is clearedd")
+             console.log("token is cleareddd")
               return res.status(200).json({ message: "Logout successful" });
 
         }catch(err){
